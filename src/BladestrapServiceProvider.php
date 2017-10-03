f8d2f2ee6ae64562978d1979b617f993be508230
Jacob Landry <jacoblandry@Jacobs-Air.hsd1.ca.comcast.net>
2017-10-02 20:31:49 -0700

    testing out ways to pass more complex data to blade directives. would prefer something a little more descriptive but I want execution to remain simple

--- src/BladestrapServiceProvider.php
+++ src/BladestrapServiceProvider.php
@@ -22,7 +22,16 @@ class BladestrapServiceProvider extends ServiceProvider
         });
 
         Blade::directive('alert', function ($expression) {
-            return Element::alert($expression);
+            // parse expression (really only helpful for debug purposes)
+            $expression = explode(",", $expression);
+            $content = trim($expression[0]);
+            $level = isset($expression[1]) ? trim($expression[1]) : 'primary';
+            $dismissable = isset($expression[2]) ? trim($expression[2]) : false;
+            $animation = isset($expression[3]) ? trim($expression[3]) : 'fade show';
+            $classes = isset($expression[4]) ? trim($expression[4]) : '';
+            $ids = isset($expression[5]) ? trim($expression[5]) : '';
+
+            return Element::alert($content, $level, $dismissable, $animation, $classes, $ids);
         });
     }
 
--- src/models/Element.php
+++ src/models/Element.php
@@ -12,29 +12,10 @@ class Element extends Model
      * @param $expression
      * @return string
      */
-    public static function alert($expression)
+    public static function alert($content, $level = 'primary', $dismissable = false, $animation = '', $classes = '', $ids = '')
     {
-        // separate options
-        $options = explode(",", $expression);
-
-        // grab content
-        if(isset($options[0])) {
-            $content = trim($options[0]);
-        }
-        else {
-            $content = '';
-        }
-
-        // grab alert level
-        if(isset($options[1])) {
-            $level = trim($options[1]);
-        }
-        else {
-            $level = 'primary';
-        }
-
         // dismissable or not
-        if(isset($options[2])) {
+        if($dismissable) {
             $dismissable = 'alert-dismissable';
             $button = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
         }
@@ -43,15 +24,7 @@ class Element extends Model
             $button = '';
         }
 
-        // animation (if dismissable)
-        if(isset($options[3])) {
-            $animation = trim($options[3]);
-        }
-        else {
-            $animation = '';
-        }
-
         // output html for alert
-        return "<?php echo '<div class=\"alert alert-$level $dismissable $animation\">{$button}{$content}</div>'; ?>";
+        return "<?php echo '<div id=\"$ids\" class=\"alert alert-$level $dismissable $animation $classes\">{$button}{$content}</div>'; ?>";
     }
 }
